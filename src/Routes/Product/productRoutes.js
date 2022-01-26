62669adec74be15d37a368b13b23eca91eeb16ba
Vitaopb <victor.lieuthier@gmail.com>
2022-01-26 14:29:17 -0300

    ft(inserting-CRUD-products)

--- src/Routes/Product/productRoutes.js
+++ src/Routes/Product/productRoutes.js
@@ -8,5 +8,8 @@ const authToken = new AuthToken();
 
 productRoutes.post('/product/user/:id', authToken.checkToken, productController.create);
 productRoutes.get('/products/user/:id', authToken.checkToken, productController.listAll);
-productRoutes.get('/product/:id', authToken.checkToken, productController.listOne);
+productRoutes.get('/product/user/:id/:barcode', authToken.checkToken, productController.listOne);
+productRoutes.put('/product/user/:id/:barcode', authToken.checkToken, productController.update);
+productRoutes.delete('/product/user/:id/:barcode', authToken.checkToken, productController.delete);
+
 export { productRoutes };
